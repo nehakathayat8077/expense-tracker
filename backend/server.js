dcb5a1bbdebb5f40e45a9fb960536dbeca577665
Neha Kathayat <nehakathayat8077@gmail.com>
2026-05-17 20:34:27 +0530

    fix: move dotenv config to top, fix route order

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./db/config');
 const authRouter=require('./routes/auth')
@@ -10,16 +12,13 @@ const cookieParser = require('cookie-parser')
 
 const app= express()
 
-dotenv.config()
+
 // and in middlewares section
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors())
 connectDB()
 
-app.listen(3000,()=>{
-    console.log("port 3000 server running")
-})
 
 app.get('/',(req,res)=>{
     res.send("api running")
@@ -27,3 +26,7 @@ app.get('/',(req,res)=>{
 
 app.use('/api/auth',authRouter)
 app.use('/api/transaction',transactionRouter)
+
+app.listen(3000,()=>{
+    console.log("port 3000 server running")
+})
